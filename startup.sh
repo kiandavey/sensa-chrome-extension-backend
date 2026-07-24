3eca60a5811d210c96bb773f8c68aba363a81238
Kian Davey <kiandaveyantolin@gmail.com>
2026-07-24 13:03:17 +0800

    chore: remove custom deployment and startup scripts for clean default deployment

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -d "sensa-chrome-extension-backend" ]; then
-  cd sensa-chrome-extension-backend
-elif [ -d "sensa-backend" ]; then
-  cd sensa-backend
-fi
-
-npm install
-node server.js
